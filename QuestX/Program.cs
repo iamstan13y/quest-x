57db871bbdb7c42dfb74366a7300430e53e7c68c
Keith Stanley <iamstan13y@gmail.com>
2024-11-22 08:18:28 +0200

    Add PDF generation setup and service registration
    Added `QuestPDF.Infrastructure` and `QuestX.Services` namespaces to `Program.cs`. Initialized the web application builder with `WebApplication.CreateBuilder(args)`. Set the QuestPDF license type to `Community`. Registered `PdfService` as a singleton service. Modified `app.Run()` to ensure the application starts correctly.

--- QuestX/Program.cs
+++ QuestX/Program.cs
@@ -1,6 +1,12 @@
+using QuestPDF.Infrastructure;
+using QuestX.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+QuestPDF.Settings.License = LicenseType.Community;
+
 // Add services to the container.
+builder.Services.AddSingleton<PdfService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
